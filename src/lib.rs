2be4b6771b78bfe580a25c683cfe4db82f54ed38
divyansh1398 <141811610+divyansh1398@users.noreply.github.com>
2024-04-02 15:07:21 +0530

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+use anchor_lang::prelude::*;
+
+// This is your program's public key and it will update
+// automatically when you build the project.
+declare_id!("DzJrzEtmeFVd2e4ks3M6rEDeRJWrgSKBYt6krv6PAqye");
+
+#[program]
+pub mod counter_program {
+    use super::*;
+
+    pub fn create(ctx: Context<Create>) -> Result<()> {
+        ctx.accounts.counter.authority = ctx.accounts.authority.key();
+        ctx.accounts.counter.count = 0;
+        Ok(())
+    }
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        ctx.accounts.counter.count += 1;
+        let ss = &ctx.accounts.counter.count.to_string()[..];
+        msg!(ss);
+        Ok(())
+    }
+    pub fn decrement(ctx: Context<Decrement>) -> Result<()> {
+        ctx.accounts.counter.count -= 1;
+        let ss = &ctx.accounts.counter.count.to_string()[..];
+        msg!(ss);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer = authority, space = 8 + 40)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut, has_one = authority)]
+    pub counter: Account<'info, Counter>,
+    pub authority: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Decrement<'info> {
+    #[account(mut, has_one = authority)]
+    pub counter: Account<'info, Counter>,
+    pub authority: Signer<'info>,
+}
+
+#[account]
+pub struct Counter {
+    pub authority: Pubkey,
+    pub count: u64,
+}
